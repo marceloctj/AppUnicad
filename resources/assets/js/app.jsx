dd55e0fba7dcfa9d0dd3a0c6816e876f3f2383f3
Marcelo Castro <marceloctj@gmail.com>
2018-09-30 02:56:00 -0300

    Retirando o carregamento do Service Worker

--- resources/assets/js/app.jsx
+++ resources/assets/js/app.jsx
@@ -2,9 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./components/App";
 
-import registerServiceWorker from './registerServiceWorker';
-
 if (document.getElementById('app-react')) {
     ReactDOM.render(<App />, document.getElementById('app-react'));
 }
-registerServiceWorker();
+//registerServiceWorker();
